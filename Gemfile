4a88a8e798364aa19957e71242885fc3d373089e
Pete Lacey <placey@wanderingbarque.com>
2013-12-07 15:09:19 -0500

    Initial commit. Base Rails and Neography

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+gem 'rails'
+gem 'neography'
+gem 'sass-rails'
+gem 'haml'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tromssa::Application.config.session_store :cookie_store, key: '_tromssa_session'
